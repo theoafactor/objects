2f4ba17eb527fe9968b2cb8cecc374ca83df85d0
Olu Adeyemo <theoafactor@gmail.com>
2025-08-01 12:28:28 +0100

    Working with  Decorators

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-import app
+def updateGreeting(func):
+
+
+    def runDecorator():
+        print("Decorator is running")
+        print("Welcome to the class this morning")
+
+
+    def getAnotherGreeting():
+        print("This is another greeting")
+   
+
+    result = func()
+    if result == 1:
+        return runDecorator
+    else: 
+        return getAnotherGreeting
+    
+
+
+
+
+@updateGreeting
+def greetUser():
+    print("Welcome to the class")
+    return 1
+    
+
+greetUser()
